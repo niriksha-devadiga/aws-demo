50dec562fdbc6f964818f4de2928e85b3e3ca142
niru <nirikshadevadiga06@gmail.com>
2025-09-12 00:45:51 +0530

    Add minimal Node.js app files

--- app/index.js
+++ app/index.js
@@ -1,8 +1,1 @@
-const http = require("http");
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.end("Hello from FolioxPixel Demo App!");
-});
-
-server.listen(port, () => console.log(`Server running on ${port}`));
+console.log(Hello from AWS ECS Demo App!);
